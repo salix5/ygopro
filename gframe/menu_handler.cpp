bfc2b317c043219fdaf96bcd8b0c2457a58daac7
salix5 <salix5566@gmail.com>
2025-08-25 21:19:24 +0800

    Refactor deck to use CardDataC pointers

--- gframe/menu_handler.cpp
+++ gframe/menu_handler.cpp
@@ -20,11 +20,11 @@ void UpdateDeck() {
 	BufferIO::Write<int32_t>(pdeck, static_cast<int32_t>(deckManager.current_deck.main.size() + deckManager.current_deck.extra.size()));
 	BufferIO::Write<int32_t>(pdeck, static_cast<int32_t>(deckManager.current_deck.side.size()));
 	for(size_t i = 0; i < deckManager.current_deck.main.size(); ++i)
-		BufferIO::Write<uint32_t>(pdeck, deckManager.current_deck.main[i]->first);
+		BufferIO::Write<uint32_t>(pdeck, deckManager.current_deck.main[i]->code);
 	for(size_t i = 0; i < deckManager.current_deck.extra.size(); ++i)
-		BufferIO::Write<uint32_t>(pdeck, deckManager.current_deck.extra[i]->first);
+		BufferIO::Write<uint32_t>(pdeck, deckManager.current_deck.extra[i]->code);
 	for(size_t i = 0; i < deckManager.current_deck.side.size(); ++i)
-		BufferIO::Write<uint32_t>(pdeck, deckManager.current_deck.side[i]->first);
+		BufferIO::Write<uint32_t>(pdeck, deckManager.current_deck.side[i]->code);
 	DuelClient::SendBufferToServer(CTOS_UPDATE_DECK, deckbuf, pdeck - deckbuf);
 }
 bool MenuHandler::OnEvent(const irr::SEvent& event) {
