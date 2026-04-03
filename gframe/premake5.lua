510a3cbd7112324a8cc07b78587aa7155056d393
salix5 <salix5566@gmail.com>
2026-04-03 18:10:42 +0800

    Move _IRR_STATIC_LIB_ to premake5.lua (#40)
    * Move _IRR_STATIC_LIB_ to premake5.lua
    
    * Remove _IRR_STATIC_LIB_ from CGUITTFont.cpp

--- gframe/premake5.lua
+++ gframe/premake5.lua
@@ -6,6 +6,7 @@ project "YGOPro"
     rtti "Off"
     openmp "On"
 
+    defines { "_IRR_STATIC_LIB_" }
     files { "*.cpp", "*.h" }
     includedirs { "../ocgcore" }
     links { "ocgcore", "clzma", LUA_LIB_NAME, "sqlite3", "irrlicht", "freetype", "event" }
