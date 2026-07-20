dd99a8ebc0b0f62015cafa9de6448a67cd96e0e8
salix5 <salix5566@gmail.com>
2026-07-20 20:35:10 +0800

    Rewrite class FileSystem

--- gframe/file_system.h
+++ gframe/file_system.h
@@ -1,31 +1,28 @@
 #ifndef FILESYSTEM_H
 #define FILESYSTEM_H
 
-#include <cstdio>
-#include <functional>
-
-// was a function before std::fopen support UTF-8 filename on Windows
-#define myfopen std::fopen
+#include <filesystem>
+#include <type_traits>
 
 FILE* mywfopen(const wchar_t* filename, const char* mode);
 
 class FileSystem {
 public:
 	static void SafeFileName(wchar_t* wfile);
-	static bool IsFileExists(const wchar_t* wfile);
-	static bool IsFileExists(const char* file);
-	static bool IsDirExists(const wchar_t* wdir);
-	static bool IsDirExists(const char* dir);
-	static bool MakeDir(const wchar_t* wdir);
-	static bool MakeDir(const char* dir);
-	static bool Rename(const wchar_t* woldname, const wchar_t* wnewname);
-	static bool Rename(const char* oldname, const char* newname);
-	static bool DeleteDir(const wchar_t* wdir);
-	static bool DeleteDir(const char* dir);
-	static bool RemoveFile(const wchar_t* wfile);
-	static bool RemoveFile(const char* file);
-	static void TraversalDir(const wchar_t* wpath, const std::function<void(const wchar_t*, bool)>& cb);
-	static void TraversalDir(const char* path, const std::function<void(const char*, bool)>& cb);
+	static bool IsFileExists(const std::filesystem::path& path);
+	static bool IsDirExists(const std::filesystem::path& path);
+	static bool MakeDir(const std::filesystem::path& path);
+	static bool Rename(const std::filesystem::path& oldpath, const std::filesystem::path& newpath);
+	static bool DeleteDir(const std::filesystem::path& path);
+	static bool RemoveFile(const std::filesystem::path& path);
+	template<typename Callback>
+	static void TraversalDir(const std::filesystem::path& path, Callback&& cb) {
+		static_assert(std::is_invocable_v<Callback, const std::filesystem::path&, bool>, "Callback must be invocable with (const std::filesystem::path&, bool)");
+		std::error_code ec;
+		for(auto it = std::filesystem::directory_iterator(path, ec); it != std::filesystem::directory_iterator{}; it.increment(ec)) {
+			cb(it->path(), it->is_directory(ec));
+		}
+	}
 };
 
 #endif //FILESYSTEM_H
